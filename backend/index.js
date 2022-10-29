d1796576f7b0cbd8b9a2f3de9286d078959b0cbc
Wade Graves <wadegraves940@gmail.com>
2022-10-29 11:17:54 -0400

    added jwt

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,22 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+const cookieSession = require('cookie-session')
 
 // Express Settings
-app.use(cors())
+app.use(cookieSession({
+    name: 'session',
+    keys: [ process.env.SESSION_SECRET ],
+    sameSite: 'strict',
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use('/authentication', require('./controllers/authentication'))
 
 // Controllers & Routes
 
@@ -18,8 +27,11 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
 })
+
+
